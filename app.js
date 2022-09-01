4ea0433e71e00936655004562af1a54cd41df59a
Kanon-Hosen <freelancerkanon1@gmail.com>
2022-09-01 17:35:38 +0600

    all Done

--- app.js
+++ app.js
@@ -1,16 +1,58 @@
+const section = document.getElementById('section');
 const loadData = async () => {
-    const response = await fetch('https://fakestoreapi.com/products');
-    const data = await response.json();
-    console.log(data)
+    const url = await fetch('https://fakestoreapi.com/products');
+    const data = await url.json();
     return data;
-    
-}
-const mainData = loadData();
+};
 
-const displayData = () => {
+
+const display = async() => {
+    const mainData = await loadData();
     mainData.forEach(item => {
-        const title = item.title;
-        const img = item.image;
-        const des = item.category;
-    });
+        const div = document.createElement('div');
+        div.classList.add('box');
+        div.innerHTML = `
+            <div class="img">
+                <img src ="${item.image}">
+            </div>
+            <div class="text">
+                <h6>${item.category}</h6>
+                <h5>${item.title.slice(0 , 22)}</h5>
+                <p>${item.price}</p>
+            </div>
+        `
+        section.appendChild(div)
+    })
 }
+display();
+const searchField = document.getElementById('searchField')
+searchField.addEventListener('keypress', async (e) => {
+    if (e.key === 'Enter') {
+        section.textContent = '';
+        const data = await loadData()
+        const foundProducts = data.filter(product => product.category.includes(searchField.value.toLowerCase()));
+        console.log(foundProducts)
+        if (foundProducts.length ) {
+            foundProducts.forEach(products => {
+                const div = document.createElement('div');
+                div.classList.add('box');
+                div.innerHTML = `
+                    <div class="img">
+                        <img src ="${products.image}">
+                    </div>
+                    <div class="text">
+                        <h6>${products.category}</h6>
+                        <h5>${products.title.slice(0 , 22)}</h5>
+                        <p>${products.price}</p>
+                    </div>
+                `
+                section.appendChild(div)
+            })
+        }
+        else {
+            section.innerHTML = `
+                <h1>No Products Found</h1>
+            `
+        }
+    }
+})
